c356b2d36ed98c7751400e36b2124cdbea1ac14d
Connor <61102529+Metater@users.noreply.github.com>
2023-12-02 00:38:58 -0500

    Prepare for day 3

---
+++ Days/Day3/Part1.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AdventOfCode2023.Days.Day3;
+
+internal class Part1 : DayPart
+{
+    public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+
+    public override void Run(List<string> input)
+    {
+
+    }
+}
---
+++ Days/Day3/Part2.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode2023.Days.Day3;
+
+internal class Part2 : DayPart
+{
+    public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+
+    public override void Run(List<string> input)
+    {
+
+    }
+}
