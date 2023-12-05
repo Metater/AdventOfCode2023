3a60da6a3cf99a4acb4c71aa5dadc973c7e6fee1
Connor <61102529+Metater@users.noreply.github.com>
2023-12-04 22:15:25 -0500

    Wow, this is better than yesterday. The recursion cache is pretty cool. Complete part 1 and 2 for day 4.

--- Days/Day4/Part1.cs
+++ Days/Day4/Part1.cs
@@ -3,10 +3,59 @@
 internal class Part1 : DayPart
 {
     public override bool HasPrecedence => true;
-    public override string InputFile => "Example.txt";
+    //public override string InputFile => "Example.txt";
 
     public override void Run(List<string> input)
     {
+        int i = 0;
+        List<Card> cards = input.Select<string, Card>(l =>
+        {
+            int index = i++;
 
+            string[] numbers = l.Split(':', StringSplitOptions.TrimEntries)[1].Split('|', StringSplitOptions.TrimEntries);
+
+            HashSet<int> winning = numbers[0].Split(' ', StringSplitOptions.TrimEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse).ToHashSet();
+
+            HashSet<int> held = numbers[1].Split(' ', StringSplitOptions.TrimEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse).ToHashSet();
+
+            return new(index, winning, held);
+        }).ToList();
+
+        int points = cards.Sum(c =>
+        {
+            var matching = c.Held.Where(n => c.Winning.Contains(n)).ToList();
+
+            if (matching.Count > 0)
+            {
+                return 1 << matching.Count - 1;
+            }
+
+            return 0;
+        });
+
+        Console.WriteLine(points);
+
+        //foreach (var card in cards)
+        //{
+        //    Console.WriteLine(card.Index);
+
+        //    Console.WriteLine("Winning:");
+        //    foreach (var w in card.Winning)
+        //    {
+        //        Console.WriteLine($"\t{w}");
+        //    }
+
+        //    Console.WriteLine("Held:");
+        //    foreach (var h in card.Held)
+        //    {
+        //        Console.WriteLine($"\t{h}");
+        //    }
+        //}
     }
+
+    record Card(int Index, HashSet<int> Winning, HashSet<int> Held);
 }
--- Days/Day4/Part2.cs
+++ Days/Day4/Part2.cs
@@ -3,10 +3,50 @@
 internal class Part2 : DayPart
 {
     public override bool HasPrecedence => true;
-    public override string InputFile => "Example.txt";
+    //public override string InputFile => "Example.txt";
 
     public override void Run(List<string> input)
     {
+        int cardIndex = 1;
+        List<Card> cards = input.Select<string, Card>(l =>
+        {
+            string[] numbers = l.Split(':', StringSplitOptions.TrimEntries)[1].Split('|', StringSplitOptions.TrimEntries);
 
+            HashSet<int> winning = numbers[0].Split(' ', StringSplitOptions.TrimEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse).ToHashSet();
+
+            HashSet<int> held = numbers[1].Split(' ', StringSplitOptions.TrimEntries)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(int.Parse).ToHashSet();
+
+            var matches = held.Where(n => winning.Contains(n)).Count();
+
+            return new(cardIndex++, matches);
+        }).ToList();
+
+        cards.Reverse();
+
+        // index, count
+        Dictionary<int, int> cache = [];
+
+        int count = 0;
+        foreach (var card in cards)
+        {
+            int recursiveContribution = 1;
+            for (int i = 1; i <= card.Matches; i++)
+            {
+                int index = card.Index + i;
+                recursiveContribution += cache[index];
+            }
+
+            cache[card.Index] = recursiveContribution;
+
+            count += recursiveContribution;
+        }
+
+        Console.WriteLine(count);
     }
+
+    record Card(int Index, int Matches);
 }
