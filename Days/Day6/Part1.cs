da160254b1af516433ce544247783004c48caf49
Connor <61102529+Metater@users.noreply.github.com>
2023-12-07 18:15:25 -0500

    Complete day 6, set up day 7

--- Days/Day6/Part1.cs
+++ Days/Day6/Part1.cs
@@ -2,12 +2,81 @@
 
 internal class Part1 : DayPart
 {
-    public override bool HasPrecedence => true;
+    //public override bool HasPrecedence => true;
     //public override string InputFile => "Example.txt";
     //public override bool ShouldRejectWhiteSpaceLines => false;
 
     public override void Run(List<string> input)
     {
+        var times = input[0].Split(':', StringSplitOptions.TrimEntries)[1]
+            .Split(' ', StringSplitOptions.TrimEntries)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(int.Parse).ToList();
+        var distances = input[1].Split(':', StringSplitOptions.TrimEntries)[1]
+            .Split(' ', StringSplitOptions.TrimEntries)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(int.Parse).ToList();
 
+        List<Race> races = [];
+
+        for (int i = 0; i < times.Count; i++)
+        {
+            races.Add(new Race(times[i], distances[i]));
+        }
+
+        //times.ForEach(Console.WriteLine);
+        //distances.ForEach(Console.WriteLine);
+        //races.ForEach(Console.WriteLine);
+
+        List<int> waysToBeatRecordForEachRace = [];
+        foreach (var race in races)
+        {
+            int currentRecordMillimeters = race.Dist;
+            int waysToBeatRecord = 0;
+
+            for (int i = 0; i < race.Time; i++)
+            {
+                int releaseButtonAtMilliseconds = i;
+
+                int positionMillimeters = 0;
+                int speedMillimetersPerMillisecond = 0;
+                for (int j = 0; j < race.Time; j++)
+                {
+                    int millisecond = j;
+
+                    if (millisecond >= releaseButtonAtMilliseconds)
+                    {
+                        positionMillimeters += speedMillimetersPerMillisecond;
+                    }
+                    else
+                    {
+                        speedMillimetersPerMillisecond++;
+                    }
+                }
+
+                if (positionMillimeters > currentRecordMillimeters)
+                {
+                    waysToBeatRecord++;
+                }
+            }
+
+            waysToBeatRecordForEachRace.Add(waysToBeatRecord);
+        }
+
+        int product = 1;
+        foreach (var item in waysToBeatRecordForEachRace)
+        {
+            product *= item;
+        }
+
+        Console.WriteLine(product);
+    }
+
+    record Race(int Time, int Dist)
+    {
+        public override string ToString()
+        {
+            return $"Time:{Time}|Dist:{Dist}";
+        }
     }
 }
--- Days/Day6/Part2.cs
+++ Days/Day6/Part2.cs
@@ -2,12 +2,44 @@
 
 internal class Part2 : DayPart
 {
-    public override bool HasPrecedence => true;
+    //public override bool HasPrecedence => true;
     //public override string InputFile => "Example.txt";
     //public override bool ShouldRejectWhiteSpaceLines => false;
 
     public override void Run(List<string> input)
     {
+        long time = Parse(input[0]);
+        long recordDistance = Parse(input[1]);
+        //Console.WriteLine(time + " " + recordDistance);
 
+        Console.WriteLine((long)(GetB(time, recordDistance) - GetA(time, recordDistance)) + 1);
+    }
+
+    private static double GetA(double r, double d)
+    {
+        return -(-r + GetSqrtPart(r, d)) / 2;
+    }
+
+    private static double GetB(double r, double d)
+    {
+        return -(-r - GetSqrtPart(r, d)) / 2;
+    }
+
+    private static double GetSqrtPart(double r, double d)
+    {
+        return Math.Sqrt(Math.Pow(r, 2) - (4 * d));
+    }
+
+    //private static long GetDistance(long time, long buttonReleaseMs)
+    //{
+    //    long travellingTime = time - buttonReleaseMs;
+    //    long speed = buttonReleaseMs;
+    //    return travellingTime * speed;
+    //}
+
+    private static long Parse(string line)
+    {
+        line = line.Split(':', StringSplitOptions.TrimEntries)[1];
+        return long.Parse(line.Replace(" ", ""));
     }
 }
