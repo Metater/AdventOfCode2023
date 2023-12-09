226e09f073d4dfd21b7a68f6733bb07864fcb337
Connor <61102529+Metater@users.noreply.github.com>
2023-12-09 12:48:29 -0500

    Prepare for day 10

---
+++ Days/Day10/Part2.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode2023.Days.Day10;
+
+internal class Part2 : DayPart
+{
+    //public override bool HasPrecedence => true;
+    //public override string InputFile => "Example.txt";
+    //public override bool ShouldRejectWhiteSpaceLines => false;
+
+    public override void Run(List<string> input)
+    {
+
+    }
+}
